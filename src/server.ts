bae65794b21b241f770e81ef0854d5bc4a199e40
youknowmeright <rakinkhan1001@gmail.com>
2026-05-16 02:26:28 +0600

    server re initialized

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
 import { createServer, IncomingMessage, type Server } from "node:http";
+import { sendResponse } from "./utilitis";
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log("Received request");
-    res.end("Hello World");
+const server: Server = createServer((req, res) => {
+   
+const url = req.url ?? "/";
+    if (url === "/") {
+        sendResponse(res, { message: "Hello, World!" });
+        return;
+    }
+    else {
+        sendResponse(res, { error: true, message: "Not Found" }, 404);
+    }
+
+
+
+
+
+
 });
 
 server.listen(5000, () => {
---
+++ src/utilitis.ts
@@ -0,0 +1,13 @@
+import type { Res } from "./types";
+
+export function sendResponse<T>(res:Res,{message,data,error}: { message?: string; data?: T; error?: boolean },status: number=200) 
+{
+    res.writeHead(status,{"content-type":"application/json"})
+    res.end(JSON.stringify({
+        
+        success:error ? false: true,
+        message:message,
+        data:error ?null :data
+     }));
+
+}
